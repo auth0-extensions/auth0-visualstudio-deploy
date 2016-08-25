d3fe08311105a19f17ca516f680e9267e095d55b
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2016-08-25 13:03:53 +0300

    qa build

--- webtask.js
+++ webtask.js
@@ -2,10 +2,8 @@ const tools = require('auth0-extension-tools');
 
 const config = require('./server/lib/config');
 const expressApp = require('./server');
-const logger = require('./server/lib/logger');
 
 module.exports = tools.createExpressServer(function(req, configProvider) {
-  logger.info('Starting Delegated Administration extension - Version:', configProvider('CLIENT_VERSION'));
   config.setProvider(configProvider);
   return expressApp();
 });

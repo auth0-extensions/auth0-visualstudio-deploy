50fd50accdee81cda2666e9ca88668d09abd3447
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2018-08-13 16:06:05 +0300

    disableHostCheck

--- build/webpack/server.js
+++ build/webpack/server.js
@@ -7,7 +7,7 @@ const logger = require('../../server/lib/logger');
 logger.info('Running development webpack server...');
 
 const options = {
-  disableHostCheck: true,
+  disableHostCheck: false,
   publicPath: 'http://localhost:3000/app/',
   hot: true,
   inline: true,
